58250f08a72c5778f9d910d49245a78486b1fe8b
Omkar Raut <omkarraut1312@gmail.com>
2023-01-13 14:08:08 +0530

    IT Assignments

---
+++ Networking/client.c
@@ -0,0 +1,54 @@
+#include"wrapper.h"
+
+int main(int argc,char** argv)
+{
+	int sockfd;
+	char buff[MAXLINE+1];
+	struct sockaddr_in servaddr;
+	char *msg = "Bye";
+
+	if(argc != 2)
+	{
+		printf("Usage : ./runcl <IPaddress>\n");
+	}
+
+	//create socket:
+	sockfd = Socket(AF_INET,SOCK_STREAM,0);
+	
+	//set port and IP the same as server-side:
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(9999);
+	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+	Connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+		
+	while(1)
+	{	
+		bzero(buff,sizeof(buff));
+		//Get input from user
+		printf("\nClient : ");
+		fgets(buff,MAXLINE,stdin);
+
+		//Send message to server
+		Send(sockfd,buff,MAXLINE,0);
+		
+		bzero(buff,sizeof(buff));
+		//Receive the server's response:
+		Recv(sockfd,buff,MAXLINE,0);
+		printf("\nServer: %s\n",buff);
+
+		if(strncmp("Bye!",buff,4)==0)
+		{
+			break;
+		}
+	}
+	
+	//Close the socket
+	close(sockfd);
+
+	printf("Bye Bye ... !!\n");
+
+	return 0;
+}
+
--- Networking/server.c
+++ Networking/server.c
@@ -3,15 +3,16 @@
 
 int main(void)
 {
-	int listenfd,connfd;
-	struct sockaddr_in servaddr;
+	int listenfd,connfd,client_size;
+	struct sockaddr_in servaddr,clientaddr;
 	char buff[MAXLINE];
-	time_t ticks;
 	
+
 	//Create socket
 	listenfd = Socket(AF_INET,SOCK_STREAM,0);
 
 	bzero(&servaddr,sizeof(servaddr));
+	bzero(&clientaddr,sizeof(clientaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(9999);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -21,21 +22,39 @@ int main(void)
 
 	//Listen for clients:
 	Listen(listenfd,LISTENQ);
+	
+	client_size = sizeof(clientaddr);
+	//Accept an incoming connection
+	connfd = Accept(listenfd,(struct sockaddr*)&clientaddr,&client_size);
+		
+	printf("Client connected at IP: %s and port: %i\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+		
 
 	while(1)
 	{
-		//Accept an incoming connection
-		connfd = accept(listenfd,(struct sockaddr*)NULL,NULL);
-		ticks= time(NULL);
-		snprintf(buff,sizeof(buff),"%s",ctime(&ticks));
+		bzero(buff,sizeof(buff));
+		//Receive client's message
+		Recv(connfd,buff,MAXLINE,0);
+		printf("\nClient : %s\n",buff);
+		
+		bzero(buff,sizeof(buff));
+		//Get input from user
+		printf("\nServer: ");
+		fgets(buff,MAXLINE,stdin);
 
 		//Respond to client 
-		write(connfd,buff,MAXLINE);
+		Send(connfd,buff,MAXLINE,0);
 
-		close(connfd);
+		if(strncmp("Bye!",buff,4) == 0)
+		{
+			break;
+		}
 	}
 	
+	close(connfd);
 	close(listenfd);
+
+	printf("Client Says : Bye !!\n");
 
 	return 0;
 }
